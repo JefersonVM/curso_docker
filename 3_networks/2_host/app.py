217686c97a4067bb950835d89d89dce399d8ec2a
Jeferson Vieira <jeferson.vm1981@gmail.com>
2024-02-14 01:45:33 -0300

    Concluíndo a seção docker compose

--- 3_networks/2_host/app.py
+++ 3_networks/2_host/app.py
@@ -1,35 +1,42 @@
 import flask
 from flask import request, json, jsonify
 import requests
-import flask_mysqldb
-from flask_mysqldb import MySQL
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-app.config['MYSQL_HOST'] = 'host.docker.internal'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flaskhost'
+app.config['DB_HOST'] = 'host.docker.internal'
+app.config['DB_USER'] = 'postgres'
+app.config['DB_PASSWORD'] = '18020526'
+app.config['DB_NAME'] = 'flaskhost'
 
-mysql = MySQL(app)
+def get_db():
+    conn = psycopg2.connect(
+        dbname=app.config['DB_NAME'],
+        user=app.config['DB_USER'],
+        password=app.config['DB_PASSWORD'],
+        host=app.config['DB_HOST']
+    )
+    return conn
 
 @app.route("/", methods=["GET"])
 def index():
-  data = requests.get('https://randomuser.me/api')
-  return data.json()
+    data = requests.get('https://randomuser.me/api')
+    return data.json()
 
 @app.route("/inserthost", methods=['POST'])
 def inserthost():
-  data = requests.get('https://randomuser.me/api').json()
-  username = data['results'][0]['name']['first']
+    data = requests.get('https://randomuser.me/api').json()
+    username = data['results'][0]['name']['first']
 
-  cur = mysql.connection.cursor()
-  cur.execute("""INSERT INTO users(name) VALUES(%s)""", (username,))
-  mysql.connection.commit()
-  cur.close()
-
-  return username
+    conn = get_db()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute("""INSERT INTO users(name) VALUES(%s)""", (username,))
+    conn.commit()
+    cur.close()
+    conn.close()
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True, port="5000")
+    app.run(host='0.0.0.0', port=5000)
